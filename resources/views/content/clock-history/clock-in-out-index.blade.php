1021c85ba9b8185febb4723006f4c9ec0e7da4ad
shaju@fujisakuratech.com <shaju@fujisakuratech.com>
2024-01-02 23:34:38 +0530

    Clock-in and out popups

--- resources/views/content/clock-history/clock-in-out-index.blade.php
+++ resources/views/content/clock-history/clock-in-out-index.blade.php
@@ -13,11 +13,11 @@
 <div class="card">
   <div class="row">
     <div class="col-md-6 ">
-      <div class="d-grid gap-3 mx-3 my-5">
-        <button type="button" class="btn btn-success py-4 fs-5"><i class='bx bx-play-circle fs-3 me-1'></i>CLOCK-IN</button>
-        <button type="button" class="btn btn-secondary py-4 fs-5"><i class='bx bx-pause-circle fs-3 me-1'></i>PAUSE WORK</button>
-        <button type="button" class="btn btn-danger py-4 fs-5"><i class='bx bx-stop-circle fs-3 me-1'></i>CLOCK-OUT</button>
-      </div>
+        <div class="d-grid gap-3  mx-3 my-5">
+            <button type="button" class="btn btn-success py-4 fs-5" data-bs-toggle="offcanvas" data-bs-target="#clockInCanvas" ><i class='bx bx-play-circle fs-3 me-1'></i>CLOCK-IN</button>
+            <button type="button" class="btn btn-warning py-4 fs-5" data-bs-toggle="offcanvas" data-bs-target="#pauseWorkCanvas" ><i class='bx bx-pause-circle fs-3 me-1' ></i>PAUSE WORK</button>
+            <button type="button" class="btn btn-danger py-4 fs-5" data-bs-toggle="offcanvas" data-bs-target="#clockOutCanvas" ><i class='bx bx-stop-circle fs-3 me-1' ></i>CLOCK-OUT</button>
+        </div>
     </div>
     <div class="col-md-6" style="box-shadow:-6px 0 10px -4px #999; ">
       <div class="row">
@@ -48,4 +48,107 @@
     </div>
   </div>
 @endif
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="clockInCanvas" aria-labelledby="clockInCanvasLabel" data-bs-scroll="true" style="overflow-y: auto">
+  <form class="needs-validation" novalidate>
+  @csrf
+      <div class="offcanvas-header">
+          <h5 id="clockInCanvasLabel" class="offcanvas-title">Clock In</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body my-auto mx-0 flex-grow-0">
+          <div class="row">
+              <div class="col mb-3">
+              <label for="name" class="form-label">Worker</label>
+              <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="Alex" disabled required>
+              <div class="invalid-feedback">Please fill out the name</div>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col mb-3">
+              <label for="comment" class="form-label">Comment</label>
+              <textarea type="text" id="comment" name="comment" class="form-control" placeholder="comment" rows="3"></textarea>
+              </div>
+          </div>
+      </div>
+      <div class="offcanvas-footer text-end mx-4">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="offcanvas" aria-label="Close">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+  </form>
+</div>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="clockOutCanvas" aria-labelledby="clockOutCanvasLabel" data-bs-scroll="true" style="overflow-y: auto">
+  <form class="needs-validation" novalidate>
+  @csrf
+      <div class="offcanvas-header">
+          <h5 id="clockOutCanvasLabel" class="offcanvas-title">Clock Out</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body my-auto mx-0 flex-grow-0">
+          <div class="row">
+              <div class="col mb-3">
+              <label for="name" class="form-label">Worker</label>
+              <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="Alex" disabled required>
+              <div class="invalid-feedback">Please fill out the name</div>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col mb-3">
+              <label for="comment" class="form-label">Comment</label>
+              <textarea type="text" id="comment" name="comment" class="form-control" placeholder="comment" rows="3"></textarea>
+              </div>
+          </div>
+      </div>
+      <div class="offcanvas-footer text-end mx-4">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="offcanvas" aria-label="Close">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+  </form>
+</div>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="pauseWorkCanvas" aria-labelledby="pauseWorkCanvasLabel" data-bs-scroll="true" style="overflow-y: auto">
+  <form class="needs-validation" novalidate>
+  @csrf
+      <div class="offcanvas-header">
+          <h5 id="pauseWorkCanvasLabel" class="offcanvas-title">Pause Work</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body my-auto mx-0 flex-grow-0">
+          <div class="row">
+              <div class="col mb-3">
+              <label for="name" class="form-label">Worker</label>
+              <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="Alex" disabled required>
+              <div class="invalid-feedback">Please fill out the name</div>
+              </div>
+          </div>
+          <div class="row mb-2">
+            <div class="demo-inline-spacing d-flex justify-content-between">
+              <button type="button" class="btn btn-icon btn-outline-secondary" style="padding: 35px">
+                <span class="tf-icons bx bx-dish" style="font-size: 30px"></span>
+              </button>
+              <button type="button" class="btn btn-icon btn-outline-secondary" style="padding: 35px">
+                <span class="tf-icons bx bx-wind"  style="font-size: 30px"></span>
+              </button>
+              <button type="button" class="btn btn-icon btn-outline-secondary" style="padding: 35px">
+                <span class="tf-icons bx bx-coffee"  style="font-size: 30px"></span>
+              </button>
+              <button type="button" class="btn btn-icon btn-outline-secondary" style="padding: 35px">
+                <span class="tf-icons bx bx-collection"  style="font-size: 30px"></span>
+              </button>
+            </div>
+          </div>
+          <div class="row">
+              <div class="col mb-3">
+              <label for="reason" class="form-label">Break For</label>
+              <textarea type="text" id="reason" name="reason" class="form-control" placeholder="reason" rows="3"></textarea>
+              </div>
+          </div>
+      </div>
+      <div class="offcanvas-footer text-end mx-4">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="offcanvas" aria-label="Close">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+  </form>
+</div>
 @endsection
